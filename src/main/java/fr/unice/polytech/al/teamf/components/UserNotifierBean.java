41035f7232d6889dd6739c070085f3d66c08a2af
Jeremy Junac <jeremy.junac@etu.unice.fr>
2018-10-15 22:52:19 +0200

    Beautiful test

--- src/main/java/fr/unice/polytech/al/teamf/components/UserNotifierBean.java
+++ src/main/java/fr/unice/polytech/al/teamf/components/UserNotifierBean.java
@@ -4,18 +4,16 @@ import fr.unice.polytech.al.teamf.NotifyUser;
 import fr.unice.polytech.al.teamf.entities.User;
 import org.springframework.stereotype.Component;
 
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
 import java.io.PrintStream;
 
 @Component
 public class UserNotifierBean implements NotifyUser {
 
-    PrintStream wrt = System.out;
+    PrintStream printStream = System.out;
 
     @Override
     public void notifyUser(User user, String message) {
-        wrt.println(String.format("Send message to %s: %s", user.getName(), message));
+        printStream.println(String.format("Send message to %s: %s", user.getName(), message));
     }
 
 }
