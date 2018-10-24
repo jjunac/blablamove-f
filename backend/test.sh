22531f189fadaa2e8d62e20fd848c06a091a367b
Thomas Canava <canava.thomas@gmail.com>
2018-10-24 23:26:54 +0200

    trying to fix backend test.sh

--- backend/test.sh
+++ backend/test.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-mvn package && \
-    java -jar target/blablamove-1.0-SNAPSHOT.jar && \
-    sleep 10 && \
-    python webservice_tests.py
+set -e
+
+mvn package
+java -jar target/blablamove-1.0-SNAPSHOT.jar &
+sleep 10
+python webservice_tests.py
