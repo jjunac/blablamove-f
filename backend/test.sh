306573a89bfb345e99694e739ff9292f62b99b97
Thomas Canava <canava.thomas@gmail.com>
2018-10-24 23:21:19 +0200

    Added docker file and docker compose

--- backend/test.sh
+++ backend/test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 mvn package && \
-    java -jar target/blablamove-1.0-SNAPSHOT.jar & && \
+    java -jar target/blablamove-1.0-SNAPSHOT.jar && \
     sleep 10 && \
     python webservice_tests.py
