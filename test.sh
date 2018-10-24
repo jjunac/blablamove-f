70ddb71e7823064091ea4bf9c2e08ac5b91b5757
Thomas Canava <canava.thomas@gmail.com>
2018-10-25 00:20:28 +0200

    added missing `set -e` in bash scripts

--- test.sh
+++ test.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
+set -e
+
 for f in $(ls -d */)
 do
     if [ -f $f/test.sh ]; then
         echo "===== Entering $f ====="
         cd $f
         bash test.sh
-        return_code=$?
-        if [ $return_code -ne 0 ];then
-            exit $return_code
-        fi
         cd ..
     fi
 done
