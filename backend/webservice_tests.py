a2ccff477777b6f6160edbe100594a50cee20e38
Thomas Canava <canava.thomas@gmail.com>
2018-10-30 22:56:53 +0100

    fixed wrong number in webservice_test

--- backend/webservice_tests.py
+++ backend/webservice_tests.py
@@ -48,7 +48,7 @@ step("Erick is notified that he will take Johann's packages")
 assert_equals(2, len(request_webservice("http://localhost:8080/notification", "pullNotificationForUser", {"username": "Erick"})))
 
 step("Jeremy is notified that Johann had an accident and that Erick will take his package")
-assert_equals(1, len(request_webservice("http://localhost:8080/notification", "pullNotificationForUser", {"username": "Jeremy"})))
+assert_equals(2, len(request_webservice("http://localhost:8080/notification", "pullNotificationForUser", {"username": "Jeremy"})))
 
 step("Thomas is notified that Johann had an accident and that Erick will take his package")
 assert_equals(2, len(request_webservice("http://localhost:8080/notification", "pullNotificationForUser", {"username": "Thomas"})))
