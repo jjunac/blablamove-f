da52cf3fee7a5ca4c55370948bc2d095e982fcb0
Thomas Canava <canava.thomas@gmail.com>
2019-01-26 00:15:41 +0100

    added user-notifier to build.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -e
-for f in "chaos_monkey_java" "backend" "gateways"
+for f in "chaos_monkey_java" "backend" "gateways" "user-notifier"
 do
     echo $f
     if [ -f $f/install.sh ]; then
