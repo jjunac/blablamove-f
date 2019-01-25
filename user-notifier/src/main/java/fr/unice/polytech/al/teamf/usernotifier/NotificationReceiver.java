988e444f7599fcf4efa18afd886b2dbb89248d0e
Thomas Canava <canava.thomas@gmail.com>
2019-01-25 13:33:04 +0100

    Trying to convert the messages to json in notifier

--- user-notifier/src/main/java/fr/unice/polytech/al/teamf/usernotifier/NotificationReceiver.java
+++ user-notifier/src/main/java/fr/unice/polytech/al/teamf/usernotifier/NotificationReceiver.java
@@ -1,25 +1,34 @@
 package fr.unice.polytech.al.teamf.usernotifier;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-//@Slf4j
+@Slf4j
 @Component
 @RabbitListener(queues = "notifications")
 public class NotificationReceiver {
-//    private final NotifyUser notifyUser;
-//
-//    @Autowired
-//    public NotificationReceiver(NotifyUser notifyUser) {
-//        log.info("teeeeeeeeeeeeeeeest");
-//        this.notifyUser = notifyUser;
-//    }
-    
+    private final NotifyUser notifyUser;
+
+    @Autowired
+    public NotificationReceiver(NotifyUser notifyUser) {
+        log.info("teeeeeeeeeeeeeeeest");
+        this.notifyUser = notifyUser;
+    }
+
+
     @RabbitHandler
-    public void receive(String in) {
-        System.out.println(" [x] Received '" + in + "'");
-//        log.info(" [x] Received '" + in + "'");
-//        notifyUser.notifyUser("lol", "lol");
+    public void receive(byte[] in) {
+        String stringified = new String(in);
+        log.info(" [x] Received '" + stringified + "'");
+        try {
+            notifyUser.notifyUser("lol", "lol");
+        } catch (Exception exception) {
+            log.error(exception.toString());
+        }
     }
 }
--- user-notifier/src/main/java/fr/unice/polytech/al/teamf/usernotifier/QueuesConfig.java
+++ user-notifier/src/main/java/fr/unice/polytech/al/teamf/usernotifier/QueuesConfig.java
@@ -1,34 +1,40 @@
 package fr.unice.polytech.al.teamf.usernotifier;
 
-import com.googlecode.jsonrpc4j.spring.AutoJsonRpcServiceImplExporter;
 import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 
 
 @Configuration
 public class QueuesConfig {
 
+
     @Bean
     public Queue hello() {
         return new Queue("notifications");
     }
-    
+//
+//    @Bean
+//    public NotificationReceiver receiver1() {
+//        return new NotificationReceiver();
+//    }
+
     @Bean
-    public NotificationReceiver receiver1() {
-        return new NotificationReceiver();
+    public MessageConverter jsonConverter() {
+        return new Jackson2JsonMessageConverter();
+
     }
+
     @Bean
     public RabbitTemplate rabbitTemplate(final ConnectionFactory connectionFactory) {
         RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
-//        rabbitTemplate.setMessageConverter();
+        rabbitTemplate.setMessageConverter(jsonConverter());
         return rabbitTemplate;
     }
-    
-    @Bean
-    public AutoJsonRpcServiceImplExporter producerJackson2MessageConverter() {
-        return new AutoJsonRpcServiceImplExporter();
-    }
+
 }
