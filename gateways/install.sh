c15a7ce7caf1296b582dd9db0c1832fc97dafc47
Thomas Canava <canava.thomas@gmail.com>
2019-02-08 11:59:42 +0100

    Chaos monkey for route finder gateway (not working yet)

--- gateways/install.sh
+++ gateways/install.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
-
 source ../utils.sh
+
+set -e
 
 parse_args $@
 
 for d in $(ls -d */)
 do
-	mvn clean install -f $d/pom.xml
+    cd $d
+	if [[ $CHAOS -eq 1 ]]; then
+        mvn clean install -DskipTests -P chaos-monkey
+    else
+        mvn clean install -DskipTests
+    fi
+    cd ..
 done
